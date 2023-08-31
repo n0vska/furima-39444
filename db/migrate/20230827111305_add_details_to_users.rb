9baf9e7be972df4a8008150e520723b8364d3e04
n0vska <calm0vsk@gmail.com>
2023-08-31 21:29:58 +0900

    マイグレーションファイル修正

--- db/migrate/20230827111305_add_details_to_users.rb
+++ db/migrate/20230827111305_add_details_to_users.rb
@@ -1,6 +1,6 @@
 class AddDetailsToUsers < ActiveRecord::Migration[6.0]
   def change
-    add_column :users, :last_name, :string null: false
+    add_column :users, :last_name, :string, null: false
     add_column :users, :first_name, :string, null: false
     add_column :users, :last_name_kana, :string, null: false
     add_column :users, :first_name_kana, :string, null: false
