2e9eb8b78edb816f571ce6a074eb489db9a86c20
n0vska <calm0vsk@gmail.com>
2023-09-14 22:21:43 +0900

    商品一覧表示機能の実装

--- app/models/user.rb
+++ app/models/user.rb
@@ -9,4 +9,6 @@ class User < ApplicationRecord
   validates :first_name_kana, presence: true, format: { with: /\A[ァ-ヶー]+\z/, message: 'は全角カタカナで入力してください' }
   validates :last_name_kana, presence: true, format: { with: /\A[ァ-ヶー]+\z/, message: 'は全角カタカナで入力してください' }
   validates :birthdate, presence: true
+
+  has_many :purchase_records
 end
